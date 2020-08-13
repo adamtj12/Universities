b0a7eab9516bd1f4d185478627a596ad1a3d1373
Adam Johnston <adamjoasis@icloud.com>
2020-08-13 14:37:55 +0100

    Cell config update

--- UniversityList/Constant/UniversityCellConfig.swift
+++ UniversityList/Constant/UniversityCellConfig.swift
@@ -3,15 +3,6 @@
 import Foundation
 import UIKit
 
-struct UniversityListCellNames {
-    static let UniversityCell = "UniversityView"
-}
-
-
-struct UniversityListCellHeight {
-    static let Height = CGFloat(400)
-}
-
 struct UniversityCellConfig {
     static let UniversityCell = "UniversityView"
     static let rowHeight = CGFloat(400)
